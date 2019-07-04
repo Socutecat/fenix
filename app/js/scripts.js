4f80e6a497e73d31a99e2ba58c828137d152b752
Socutecat <dimiurged@gmail.com>
2019-07-05 01:59:00 +0300

    add 3 block

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,11 +1,35 @@
-const link = document.querySelector ('.auth-link');
-console.log(link);
+// const link = document.querySelector ('.auth-link');
+// console.log(link);
 
-const otherLink = document.querySelectorAll ('.auth-block-link-other');
-console.log(otherLink);
+// const otherLink = document.querySelectorAll ('.auth-block-link-other');
+// console.log(otherLink);
 
-link.addEventListener('click', () => {
-	otherLink.forEach ((link)=> {
-		link.classList.toggle('active')
-	})
-})
+// link.addEventListener('click', () => {
+// 	otherLink.forEach ((link)=> {
+// 		link.classList.toggle('active')
+// 	})
+// })
+
+function carousel_init() {
+	$(".owl-carousel").owlCarousel({
+		items:1,
+		nav:true,
+		loop:true,
+		dots:true,
+		autoplay:false,
+	});
+}
+
+function fancybox_init() {
+	$('[data-fancybox]').fancybox({
+		youtube : {
+			controls : 0,
+			showinfo : 0
+		},
+	});
+}
+
+$(document).ready(function(){
+	carousel_init();
+	fancybox_init();
+});
