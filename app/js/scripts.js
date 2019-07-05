c17a4f8c320dae6d4860c25bc243633936f63a6a
Socutecat <dimiurged@gmail.com>
2019-07-05 18:56:08 +0300

    changev4

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,14 +1,4 @@
-// const link = document.querySelector ('.auth-link');
-// console.log(link);
 
-// const otherLink = document.querySelectorAll ('.auth-block-link-other');
-// console.log(otherLink);
-
-// link.addEventListener('click', () => {
-// 	otherLink.forEach ((link)=> {
-// 		link.classList.toggle('active')
-// 	})
-// })
 
 
 /* scroll header */
